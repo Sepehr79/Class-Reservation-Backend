bfbe55fbedb83cbbfe05310e1bb3d443cdab1e0c
sepehr79 <sepehrmsm1379@gmail.com>
2024-10-21 22:34:30 +0330

    JWT token authentication test

--- src/test/java/com/verysoft/classreservation/ClassReservationApplicationTests.java
+++ src/test/java/com/verysoft/classreservation/ClassReservationApplicationTests.java
@@ -1,13 +1,54 @@
 package com.verysoft.classreservation;
 
+import com.verysoft.classreservation.reservation.dto.UserDto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.*;
 
-@SpringBootTest
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ClassReservationApplicationTests {
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void jwtTokenAuthenticationTest() {
+        ResponseEntity<UserDto> signupResponse = testRestTemplate.postForEntity("http://localhost:" + port + "/signup",
+                new UserDto("sepehr", "1234", List.of("MANAGER")), UserDto.class);
+        Assertions.assertEquals(HttpStatus.OK, signupResponse.getStatusCode());
+        Assertions.assertEquals("sepehr", signupResponse.getBody().getUsername());
+        Assertions.assertEquals("MANAGER", signupResponse.getBody().getRoles().get(0));
+
+        ResponseEntity<UserDto> loginResponse = testRestTemplate.postForEntity("http://localhost:" + port + "/login" ,
+                new UserDto("sepehr", "1234", List.of()), UserDto.class);
+        Assertions.assertEquals(HttpStatus.OK, loginResponse.getStatusCode());
+        String jwtCookie = loginResponse.getHeaders().getFirst(HttpHeaders.SET_COOKIE);
+        assert jwtCookie != null;
+
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.add("Cookie", jwtCookie);
+        HttpEntity requestEntity = new HttpEntity(null, requestHeaders);
+        ResponseEntity<String> protectedResponse = testRestTemplate.exchange(
+                "http://localhost:" + port + "/managers",
+                HttpMethod.GET,
+                requestEntity,
+                String.class);
+        Assertions.assertEquals(HttpStatus.OK, protectedResponse.getStatusCode());
+
+
     }
 
 }
